531a1e5cddc1fb1ab96f76a1a6addc311cb1cb88
DarshHdesai <107111028+DarshHdesai@users.noreply.github.com>
2023-11-17 14:05:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(640 , 480);
+    canvas.position(110 , 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+
+}
+
+function draw(){
+    
+    circle(30, 30, 20);
+    rect(30 , 20 ,55 , 55);
+    ellipse(56, 46, 55, 55);
+}
+
+
